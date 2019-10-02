dd25a3f76ba8997a05542518f9518d5b944cbd7f
Jose Guadalupe Omar Ramirez Mata <Jose.Ramirez@municipiodequeretaro.gob.mx>
2019-10-02 16:54:30 -0500

    Vuetify #2 completed

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-layout wrap>
+      <v-flex class="primary" xs12>
+        <h1>12</h1>
+      </v-flex>
+      <v-flex class="secondary" xs6>
+        <h1>12</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout class="mt-5 indigo lighten-2" justify-center align-center style="height:300px;">
+      <v-flex class="indigo lighten-1" xs3>
+        <h1>centrado</h1>
+      </v-flex>
+      <v-flex class="indigo darken-1 white--text" xs3 >
+        <h1>centrado</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>
+        <h1 class="display-1 text-uppercase">Lorem ipsum.</h1>
+        <h1 class="display-2 teal accent-3 pa-1 mt-5">Lorem ipsum.</h1>
+        <h1 class="display-3 text-center">Lorem ipsum.</h1>
+        <h1 class="display-4">Lorem ipsum.</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+};
+</script>
